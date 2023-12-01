bbb616be7e96aac8cc6849b022aa9b7ab793f01e
Dolin <366688603@qq.com>
2023-12-01 10:08:47 +0800

    feat:验证 iOS 安卓（iOS 需要打成 ipa 包并上传，直接启动会提示 Flutter 项目要连接手机）

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -12,113 +12,115 @@ class HomeView extends GetView<HomeController> {
       appBar: AppBar(
         title: const Text('HomeView'),
         centerTitle: true,
+        actions: [
+          InkWell(
+            child: const Center(
+              child: Text(
+                'jump 2 test page',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            onTap: () {
+              Get.toNamed(Routes.TEST);
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10.0),
-          child: Column(
-            children: [
-              // const Text(
-              //   '版本号需要与patch号相同',
-              //   style: TextStyle(
-              //     fontSize: 24,
-              //     fontWeight: FontWeight.w400,
-              //     color: Colors.red,
-              //   ),
-              // ),
-              // const Text(
-              //   'iOS 测试',
-              //   style: TextStyle(
-              //     fontSize: 24,
-              //     fontWeight: FontWeight.w600,
-              //     color: Colors.green,
-              //   ),
-              // ),
-              // const Text(
-              //   'test 单独推安卓、iOS \n iOS2 \n Android',
-              //   style: TextStyle(
-              //     fontSize: 24,
-              //     fontWeight: FontWeight.w600,
-              //     color: Colors.blue,
-              //   ),
-              // ),
-              Obx(() => Text(
-                    controller.versionInfo.value,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.orange,
-                    ),
-                  )),
-              const SizedBox(height: 20),
-              Obx(() => Text(
-                    controller.curPatchNum.value,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.orange,
-                    ),
-                  )),
-              const SizedBox(height: 20),
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // const Text(
+                //   '版本号需要与patch号相同',
+                //   style: TextStyle(
+                //     fontSize: 24,
+                //     fontWeight: FontWeight.w400,
+                //     color: Colors.red,
+                //   ),
+                // ),
+                // const Text(
+                //   'iOS 测试',
+                //   style: TextStyle(
+                //     fontSize: 24,
+                //     fontWeight: FontWeight.w600,
+                //     color: Colors.green,
+                //   ),
+                // ),
+                // const Text(
+                //   'test 单独推安卓、iOS \n iOS2 \n Android',
+                //   style: TextStyle(
+                //     fontSize: 24,
+                //     fontWeight: FontWeight.w600,
+                //     color: Colors.blue,
+                //   ),
+                // ),
+                const SizedBox(height: 20),
+                Obx(() => Text(
+                      controller.versionInfo.value,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.orange,
+                      ),
+                    )),
+                const SizedBox(height: 20),
+                Obx(() => Text(
+                      controller.curPatchNum.value,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.red,
+                      ),
+                    )),
+                const SizedBox(height: 20),
 
-              Obx(() => Text(
-                    controller.updateAvailable.value,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.green,
-                    ),
-                  )),
-              const SizedBox(height: 20),
+                Obx(() => Text(
+                      controller.updateAvailable.value,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.green,
+                      ),
+                    )),
+                const SizedBox(height: 20),
 
-              Obx(() => Text(
-                    controller.shorebirdStatus.value,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.red,
-                    ),
-                  )),
-              const SizedBox(height: 20),
+                Obx(() => Text(
+                      controller.shorebirdStatus.value,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.blue,
+                      ),
+                    )),
+                const SizedBox(height: 20),
 
-              const Spacer(),
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                  child: const Text(
-                    'jump 2 test page',
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                      color: Color(0xFF000000),
+                const Spacer(),
+
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    child: const Text(
+                      'excute checkForUpdates',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
                     ),
+                    onPressed: () {
+                      controller.checkForUpdates();
+                    },
                   ),
-                  onPressed: () {
-                    Get.toNamed(Routes.TEST);
-                  },
                 ),
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                  child: const Text(
-                    'checkForUpdates',
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                      color: Color(0xFF000000),
-                    ),
-                  ),
-                  onPressed: () {
-                    controller.checkForUpdates();
-                  },
-                ),
-              ),
-              const SizedBox(height: 20),
-            ],
+                const SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
       ),
